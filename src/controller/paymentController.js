c646c5fed5e7504514f759901fdd8f76aa8d56d4
gongsup <supurkorn@hotmail.com>
2026-01-25 21:04:18 +0700

    add logs

--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -7,6 +7,7 @@ const endpointSecret = process.env.ENPOINT_SECRET;
 const { v4: uuidv4 } = require("uuid");
 
 const orderService = require("../service/orderService");
+const { order } = require("../model/prisma");
 
 const paymentController = {};
 // stripe
@@ -40,6 +41,7 @@ paymentController.checkout = async (req, res, next) => {
       success_url: `${process.env.BASE_URL}/success.html`,
       cancel_url: `${process.env.BASE_URL}/cancel.html`,
     });
+    console.log("eoeoe");
 
     res.status(200).json({ url: session.url });
     // Frontend จะใช้ URL นี้เพื่อ redirect ผู้ใช้ไปยังหน้าชำระเงินของ Stripe
@@ -56,6 +58,8 @@ paymentController.webhook = async (req, res) => {
   try {
     // req.body = ข้อมูลเกี่ยวกับการชำระเงิน
     event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+    console.log("############ Webhook Event Received ###########");
+    console.log(event);
   } catch (err) {
     console.error("Webhook Error:", err.message);
     res.send(`Webhook Error: ${err.message}`);
@@ -81,6 +85,7 @@ paymentController.webhook = async (req, res) => {
       await paymentService.webhook(data, sessionId, uuid);
 
       await orderService.updateOrder(orderId, "payed");
+      console.log("Payment was successful.");
 
       res.status(200).send("payment success");
       break;
